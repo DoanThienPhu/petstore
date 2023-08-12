eed0babb0628f02911aab47933a921f3255d063d
DoanThienPhu <141014299+DoanThienPhu@users.noreply.github.com>
2023-08-12 14:25:50 +0700

    make it better

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'b9b68ef3329c03' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '57ff9635' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'us-cdbr-east-06.cleardb.net' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
